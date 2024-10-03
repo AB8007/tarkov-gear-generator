ffef17af8366bf8de66d6f5464449f1855ccccf2
AB8007 <AB8007@student.labranet.fi>
2024-10-03 21:15:32 +0300

    fixed a linting error

--- src/Components/RenderUIElements/SettingsMenu.test.jsx
+++ src/Components/RenderUIElements/SettingsMenu.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen, within } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Menu } from './Menu';
 import configureMockStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
